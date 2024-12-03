8e5a3dc25f1e79de592d8882bf06e3a505e647c7
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-03 20:59:13 +0530

    feat: ci/cd

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,17 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import {Router} from "@solidjs/router";
+import {FileRoutes} from "@solidjs/start/router";
+import {Suspense} from "solid-js";
 import "./app.css";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      base={import.meta.env.SERVER_BASE_URL}
+      root={(props) => (
         <main class="min-h-screen">
           <Suspense>{props.children}</Suspense>
         </main>
-      )}
-    >
+      )}>
       <FileRoutes />
     </Router>
   );
