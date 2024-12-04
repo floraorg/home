09e81254b5b6d99137748c0a4e39bcea3c76fc00
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-04 22:02:55 +0530

    fix: replace imgur

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ export default function Projects() {
     <div class="w-full my-8 flex-wrap flex justify-start">
       <div class="div w-full md:w-1/2 pb-2 pr-0 md:pb-0 md:pr-2">
         <div class="w-full flex flex-col group">
-          <img src="https://i.imgur.com/abJGvtz.png" alt="" class="w-full h-[18rem] object-cover opacity-60 group-hover:opacity-100 transition" />
+          <img src="/aster.png" alt="" class="w-full h-[18rem] object-cover opacity-60 group-hover:opacity-100 transition" />
           <div class="p-4 bg-neutral-950">
             <div class="flex justify-between">
               <p class="text-neutral-300">⭐ aster</p>
@@ -17,7 +17,7 @@ export default function Projects() {
       </div>
       <div class="div w-full md:w-1/2 pt-2 md:pt-0 pl-0 md:pl-2">
         <div class="w-full group flex flex-col">
-          <img src="https://i.imgur.com/AtDcSFO.png" class="w-full opacity-60 group-hover:opacity-100 transition h-[18rem] object-cover" alt="" />
+          <img src="/orchid.png" class="w-full opacity-60 group-hover:opacity-100 transition h-[18rem] object-cover" alt="" />
           <div class="p-4 bg-neutral-950">
             <div class="flex justify-between">
               <p class="text-neutral-300">🌺 orchid</p>
